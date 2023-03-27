15a751114d8f4bc6f594226e36abc6a413d74aff
Umang <umang965454@gmail.com>
2023-03-27 21:46:21 +0530

    Config Vercel 12

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const couponRoute = require("./Routes/couponRoute");
+const path = require("path");
 
 // Middlewares
 app.use(bodyParser.json());
@@ -12,9 +13,9 @@ app.use(cors());
 app.use("/api/v1/coupon", couponRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.use(express.static(path.join(__dirname, "/frontend", "build")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html")),
+    res.sendFile(path.join(__dirname, "/frontend", "build", "index.html")),
       function (err) {
         if (err) {
           res.status(500).send({
