a8888486ff2bf81c3b1bd5c30abe16b05fa9ba2e
Umang <umang965454@gmail.com>
2023-03-27 19:56:05 +0530

    Configuring vercel 3

--- app.js
+++ app.js
@@ -11,4 +11,11 @@ app.use(cors());
 // Routes
 app.use("/api/v1/coupon", couponRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
 module.exports = app;
